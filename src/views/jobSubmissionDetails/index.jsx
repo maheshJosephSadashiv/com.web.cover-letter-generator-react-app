6a254a6c29ca19407c13ed4a5396b77ac9c5cfe7
Abhijit Kulkarni <mailabhijitkulkarni@gmail.com>
2024-08-08 02:30:15 -0700

    Integration with API

--- src/views/jobSubmissionDetails/index.jsx
+++ src/views/jobSubmissionDetails/index.jsx
@@ -1,41 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './jobSubmissionDetails.css'
+import { Endpoints } from '../../api/endpoints';
+import axios from 'axios';
 
 const JobSubmissionDetails = () => {
+
+    // Variables
+    const [formData, setFormData] = useState({});
+    const [resultData, setResultData] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    // API Helpers
+    const sendRequest = () => {
+        console.log("Sending Request...");
+        const payload = {
+            linkedin_link: formData.linkedInLinkControlData || "",
+            name: formData.companyNameControlData,
+            address: formData.addressControlData,
+            job_description: formData.jdControlData,
+            job_role: formData.jobRoleControlData
+        };
+        setLoading(true);
+        axios.post(Endpoints.serverEndpoint, payload).then(response => {
+            console.log("Response Recieved.");
+            setResultData(response.data);
+            setLoading(false);
+        }).catch(() => {
+            console.log("Something Went Wrong!!!")
+        })
+    }
+
+    const handleDataChange = (event, key)=> {
+        setFormData({...formData, [key]: event.target.value});
+    }
+
+    // View Section
     return (
         <div className="job-submission-details">
+            {
+                loading &&
+                <div className='loader-backdrop'>
+                    <div class="loader spinner-grow text-primary" role="status" />
+                    <span className='loader-text'>Brewing Magic, Please Wait...</span>
+                </div>
+            }
             <div className="job-submission-details-form">
                 <div className="linkedin-secton">
-                    <div class="mb-3">
-                        <label for="linkedInLinkControl" class="form-label">Paste LinkedIn Link:</label>
-                        <textarea class="form-control" id="linkedInLinkControl" rows="3"></textarea>
+                    <div className="mb-3">
+                        <label htmlFor="linkedInLinkControl" className="form-label">Paste LinkedIn Link:</label>
+                        <textarea className="form-control" id="linkedInLinkControl" rows="3" onChange={(event) => handleDataChange(event, 'linkedInLinkControlData')}></textarea>
                     </div>
                 </div>
                 <h4 className='or-text'>OR</h4>
                 <div className="manual-section">
-                    <div class="mb-3">
-                        <label for="companyNameControl" class="form-label">Company Name:</label>
-                        <textarea class="form-control" id="companyNameControl" rows="3"></textarea>
+                    <div className="mb-3">
+                        <label htmlFor="companyNameControl" className="form-label">Company Name:</label>
+                        <textarea className="form-control" id="companyNameControl" rows="3" onChange={(event) => handleDataChange(event, 'companyNameControlData')}></textarea>
                     </div>
-                    <div class="mb-3">
-                        <label for="addressControl" class="form-label">Address:</label>
-                        <textarea class="form-control" id="addressControl" rows="3"></textarea>
+                    <div className="mb-3">
+                        <label htmlFor="addressControl" className="form-label">Address:</label>
+                        <textarea className="form-control" id="addressControl" rows="3" onChange={(event) => handleDataChange(event, 'addressControlData')}></textarea>
                     </div>
-                    <div class="mb-3">
-                        <label for="jdControl" class="form-label">Job Description:</label>
-                        <textarea class="form-control" id="jdControl" rows="3"></textarea>
+                    <div className="mb-3">
+                        <label htmlFor="jdControl" className="form-label">Job Description:</label>
+                        <textarea className="form-control" id="jdControl" rows="3" onChange={(event) => handleDataChange(event, 'jdControlData')}></textarea>
                     </div>
-                    <div class="mb-3">
-                        <label for="jobRoleControl" class="form-label">Job Role:</label>
-                        <textarea class="form-control" id="jobRoleControl" rows="3"></textarea>
+                    <div className="mb-3">
+                        <label htmlFor="jobRoleControl" className="form-label">Job Role:</label>
+                        <textarea className="form-control" id="jobRoleControl" rows="3" onChange={(event) => handleDataChange(event, 'jobRoleControlData')}></textarea>
                     </div>
-                    <button type="button" class="btn btn-success">Submit</button>
+                    <button type="button" className="btn btn-success" onClick={sendRequest}>Submit</button>
                 </div>
             </div>
             <div className="seperator" />
-            <div className="result">
-                Result
-            </div>
+            {
+                loading || !resultData ? <></> :
+                (
+                    <div className="result">
+                        <p className='lead'>From Address : </p>
+                        {resultData?.from_address}
+                        <p className='lead'>Address :</p>
+                        {resultData?.to_address}
+                        <p className='lead'>Introduction :</p>
+                        {resultData?.introduction}
+                        <p className='lead'>Paragraph 1 :</p>
+                        {resultData?.main_paragraph_1_prompt}
+                        <p className='lead'>Paragraph 2 :</p>
+                        {resultData?.main_paragraph_2_prompt}
+                        <p className='lead'>Conclusion :</p>
+                        {resultData?.conclusion}
+                    </div>
+                )
+            }
+            {
+                !resultData && !loading && <div className='result'><p>Search for results.</p></div>
+            }
         </div>
     );
 }
--- src/views/navbar/index.jsx
+++ src/views/navbar/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './navbar.css';
 
 const NavBar = () => {
     return (
-        <nav class="navbar bg-primary navbar-expand-lg" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">CoverLetterGen V 1.0 <sub class="beta-text">Beta</sub></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar bg-primary navbar-expand-lg" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">CoverLetterGen V 1.0 <sub className="beta-text">Beta</sub></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="/">Home</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/jobSubmissionDetails">Create Cover Letter</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/jobSubmissionDetails">Create Cover Letter</a>
                     </li>
                 </ul>
                 </div>
